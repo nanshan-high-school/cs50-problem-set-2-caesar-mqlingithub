10b41ad37764c38bc71b5b8527a66a3cd3ec9641
25019林后辰 <43640545+mqlingithub@users.noreply.github.com>
2018-12-15 08:59:09 +0800

    Update 25019caesar.cpp

--- 25019caesar.cpp
+++ 25019caesar.cpp
@@ -4,28 +4,24 @@ using namespace std;
 int main() {
   string str;
   long int key, count;
-  cout << "請輸入要加密(凱薩加密)的明文(不可有空格，會爆XD)(非字母不會加密喔!):";
+  cout << "請輸入要加密(凱薩加密)的明文:";
   cin >> str;
-  cout << "請輸入密鑰(每一字元要加多少)(同上):";
+  cout << "請輸入密鑰(每一字元要加多少):";
   cin >> key;
   cout << "\n密文為:";
-  for (int i = 0; i < count; i++) {
-    if (str[i] == '\0') count = 0;
-    else if (str[i] >= 'A' && str[i] <= 'Z') {
+  for (int i = 0; str[i] != '\0'; i++) {
+    if (str[i] >= 'A' && str[i] <= 'Z') {
       str[i] = (str[i] - 65 + key) % 26 + 65;
       cout << (char)str[i];
-      count++;
     }
 
     else if (str[i] >= 'a' && str[i] <= 'z') {
       str[i] = (str[i] - 97 + key) % 26 + 97;
       cout << (char)str[i];
-      count++;
     }
 
     else {
       cout << (char)str[i];
-      count++;
     }
   }
 }
